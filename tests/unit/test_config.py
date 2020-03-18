ddd57a538d346368f1ed7a681cccdea6a677255a
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-03-18 19:28:52 +0100

    added unit tests

---
+++ tests/unit/test_config.py
@@ -0,0 +1,95 @@
+"""
+TODO: Doku
+"""
+from configparser import ParsingError
+
+from corona_germany_telegram_bot.config import Config
+from corona_germany_telegram_bot.utility import (create_file_if_not_exists,
+                                                 delete_file_if_exists,
+                                                 write_into_file)
+from tests import unittest
+
+
+class TestConfig(unittest.TestCase):
+    """
+    TODO: Doku
+    """
+
+    def setUp(self):
+        content = """[TEST]
+        teststring = string
+        testboolean = true
+        testint = 1
+        testfloat = 1.1
+        """
+        create_file_if_not_exists("test.ini")
+        write_into_file("test.ini", content)
+
+        self.config = Config("test.ini")
+
+    def tearDown(self):
+        delete_file_if_exists("test.ini")
+
+    def test_constructor_file_not_found(self):
+        """
+        TODO: Doku
+        """
+        with self.assertRaises(FileNotFoundError):
+            Config("test2.ini")
+
+    def test_constructor_file_not_read(self):
+        """
+        TODO: Doku
+        """
+        create_file_if_not_exists("test.ini")
+        content = """[DEFAULT]
+        goodcontent = test
+        badcontent
+        """
+        write_into_file("test.ini", content)
+        with self.assertRaises(ParsingError):
+            Config("test.ini")
+
+    def test_get_value_error(self):
+        """
+        TODO: Doku
+        """
+        self.assertEqual(self.config.get_value("TEST", "notexistent"), "")
+
+    def test_get_value_bool(self):
+        """
+        TODO: Doku
+        """
+        self.assertEqual(self.config.get_value("TEST", "testboolean"), True)
+
+    def test_get_value_int(self):
+        """
+        TODO: Doku
+        """
+        self.assertEqual(self.config.get_value("TEST", "testint"), 1)
+
+    def test_get_value_float(self):
+        """
+        TODO: Doku
+        """
+        self.assertEqual(self.config.get_value(
+            "TEST", "testfloat"), float(1.1))
+
+    def test_get_sections(self):
+        """
+        TODO: Doku
+        """
+        self.assertIn("TEST", self.config.get_sections())
+
+    def test_set_value_success(self):
+        """
+        TODO: Doku
+        """
+        self.assertEqual(self.config.set_value(
+            "TEST", "teststring", "erfolg"), True)
+
+    def test_set_value_fail_section(self):
+        """
+        TODO: Doku
+        """
+        self.assertEqual(self.config.set_value("TEST2", "asd", "asd"), False)
---
+++ tests/unit/test_utility.py
@@ -0,0 +1,106 @@
+"""
+this file is the test module for the utility module
+"""
+import os
+from datetime import datetime, timedelta
+from time import sleep
+
+from corona_germany_telegram_bot.utility import (create_file_if_not_exists,
+                                                 create_folder_if_not_exists,
+                                                 csv_to_array,
+                                                 delete_file_if_exists,
+                                                 execute_os_command,
+                                                 file_exists,
+                                                 file_get_contents,
+                                                 folder_exists,
+                                                 get_abs_path_of_filepath,
+                                                 is_float, is_int, rename_file,
+                                                 time_duration_diff, urlencode,
+                                                 write_into_file)
+from tests import unittest
+
+
+class TestUtility(unittest.TestCase):
+    """
+    this class contains all test functions for the utility module
+    """
+
+    def test_folder(self):
+        """
+        TODO: Doku
+        """
+        foldername = "pytestfolder"
+        create_folder_if_not_exists(foldername)
+        sleep(1)
+        self.assertTrue(folder_exists(foldername))
+        sleep(1)
+        os.removedirs(foldername)
+        sleep(1)
+        self.assertFalse(folder_exists(foldername))
+
+    def test_file(self):
+        """
+        TODO: Doku
+        """
+        assert not file_exists("testfile")
+        create_file_if_not_exists("testfile")
+        assert file_exists("testfile")
+        write_into_file("testfile", "testcontent")
+        assert file_get_contents("testfile") == "testcontent"
+        self.assertGreater(len(get_abs_path_of_filepath("testfile")), 0)
+        rename_file("testfile", "testfilenew")
+        assert not file_exists("testfile")
+        assert file_exists("testfilenew")
+        delete_file_if_exists("testfilenew")
+        assert not file_exists("testfile")
+        assert not file_exists("testfilenew")
+
+    def test_float_int(self):
+        """
+        TODO: Doku
+        """
+        assert is_float(1)
+        assert is_float(1.1)
+        assert is_float(1.0)
+        assert not is_float("test")
+        assert is_int(1)
+        assert is_int(1.0)
+        assert not is_int(1.1)
+        assert not is_int("test")
+
+    def test_csv(self):
+        """
+        TODO: Doku
+        """
+        self.assertListEqual(csv_to_array("var1;var2", ";"), ["var1", "var2"])
+
+    def test_urlencode(self):
+        """
+        TODO: Doku
+        """
+        self.assertEqual(urlencode("asd asd +"), "asd%20asd%20%2B")
+
+    def test_time_duration_diff(self):
+        """
+        TODO: Doku
+        """
+        date1 = datetime.now()
+        date2 = date1 + timedelta(0, 5)
+        self.assertEqual(time_duration_diff(date1, date2), "5 seconds")
+        date1 = datetime.now()
+        date2 = datetime.now()
+        self.assertEqual(time_duration_diff(date1, date2), "1 second")
+
+    def test_execute_os_command(self):
+        """
+        TODO: Doku
+        """
+        self.assertTrue(execute_os_command("touch testfile"))
+        self.assertTrue(file_exists("testfile"))
+        delete_file_if_exists("testfile")
+
+    def test_execute_os_command_error(self):
+        """
+        TODO: Doku
+        """
+        self.assertFalse(execute_os_command("/bin/false"))
